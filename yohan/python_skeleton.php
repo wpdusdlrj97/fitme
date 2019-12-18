68c17477dd5d06612fbdeab845ec93a1af43e595
wpdusdlrj97 <48294315+wpdusdlrj97@users.noreply.github.com>
2019-12-18 15:33:28 +0900

    20191218 folder

--- yohan/python_skeleton.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$uploade_dir = './write_image';
-
-$name = $_FILES['myfile']['name'];
-$ext = array_pop(explode('.',$name));
-
-move_uploaded_file($_FILES['myfile']['tmp_name'],"$uploade_dir/$name");
-?>
